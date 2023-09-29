4f05c8d857298190ffe7981842e5d29d0dd1874c
Aiza0102 <isaac.daniel102@hotmail.com>
2023-09-28 20:31:30 -0500

    Tercera entrega AizaStore | Se adjunta SASS, Bootstrap, Seo, responsive y muchos mas componentes al sitio web

--- js/main.js
+++ js/main.js
@@ -33,4 +33,12 @@ closeBtn.addEventListener("click", () => {
     navigation.classList.remove("active");
 });
 
-
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+});
+  // filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+});
